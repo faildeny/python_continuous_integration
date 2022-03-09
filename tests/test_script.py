fd1ba9ea97621927c3133583171a3dbc421132a4
Grzegorz Skorupko <grzegorz.skorupko@gmail.com>
2022-03-09 10:20:42 +0100

    Add sample tests

---
+++ tests/test_script.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sample_app.script import non_zero
+
+
+def test_non_zero():
+    array = np.ones(10)
+    assert non_zero(array)
+
+    array = np.zeros(16)
+    assert not non_zero(array)
+
+    array = np.zeros(16)
+    array[4] = 5
+    assert non_zero(array)
